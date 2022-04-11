f7e711801c7b5228ae7ad52ebe36f7fd08259cba
kokkoxPL <adriangamracy@gmail.com>
2022-04-11 23:40:15 +0200

    Small changes

--- app.js
+++ app.js
@@ -2,10 +2,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const path = require('path');
-const dotenv = require('dotenv');
 const Quiz = require('./views/models/quiz');
 const app = express();
-dotenv.config();
+require('dotenv').config();
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(process.env.PORT))
